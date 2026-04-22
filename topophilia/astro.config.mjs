5891117eba9b2d06ef7c08b5b62a30d53867cba6
realtopophilia <137011514+realtopophilia@users.noreply.github.com>
2026-04-22 14:49:23 -0400

    Update astro.config.mjs

--- topophilia/astro.config.mjs
+++ topophilia/astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://topophilia.city',
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind(), mdx()],
 });
